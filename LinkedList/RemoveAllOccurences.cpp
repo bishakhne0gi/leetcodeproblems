48cf416cc4080d5c93657201a24df7842335fb0e
Bishakh Neogi <50258639+bishakhne0gi@users.noreply.github.com>
2021-12-27 20:14:32 +0530

    LinkedList

---
+++ LinkedList/RemoveAllOccurences.cpp
@@ -0,0 +1,46 @@
+/**Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+Example 2:
+
+Input: head = [], val = 1
+Output: []
+Example 3:
+
+Input: head = [7,7,7,7], val = 7
+Output: []
+**/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+       ListNode *dummy=new ListNode();
+       ListNode *temp;
+       temp=dummy;
+       dummy->next=head;
+       while(temp!= NULL && temp->next!= NULL) {
+            if(temp->next->val==val) {
+                temp->next=temp->next->next;
+            }
+            else
+                temp=temp->next;
+        }
+        return dummy->next;
+    }
+};
