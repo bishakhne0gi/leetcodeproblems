a8810cf770b0dca87752a4a4670df0bc5d3e7cd0
Bishakh Neogi <50258639+bishakhne0gi@users.noreply.github.com>
2021-12-29 22:35:41 +0530

    Stack

--- Stack/PreOrderTraversal.cpp
+++ Stack/PreOrderTraversal.cpp
@@ -1,5 +1,5 @@
 /**
-Given the root of a binary tree, return the inorder traversal of its nodes' values.
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
 
  
 
@@ -7,7 +7,7 @@ Example 1:
 
 
 Input: root = [1,null,2,3]
-Output: [1,3,2]
+Output: [1,2,3]
 Example 2:
 
 Input: root = []
@@ -19,14 +19,14 @@ Output: [1]
 **/
 
 /** ALGO
-1) Create an empty stack S.
-2) Initialize current node as root
-3) Push the current node to S and set current = current->left until current is NULL
-4) If current is NULL and stack is not empty then 
-     a) Pop the top item from stack.
-     b) Print the popped item, set current = popped_item->right 
-     c) Go to step 3.
-5) If current is NULL and stack is empty then we are done.
+Create an empty stack S.
+Initialize the current node as root.
+Push the current node to S and set current = current->left print the peek element in the stack until the current is NULL.
+If current is NULL and stack is not empty then 
+    a) Pop the top item from stack.
+    b) set current = popped_item->right.
+    c) Go to step 3.
+If the current is NULL and the stack is empty then we are done.
 **/
 /**
  * Definition for a binary tree node.
@@ -41,7 +41,7 @@ Output: [1]
  */
 class Solution {
 public:
-    vector<int> inorderTraversal(TreeNode* root) {
+    vector<int> preorderTraversal(TreeNode* root) {
     vector<int> v;
     int i=0;
     stack<TreeNode *> s;
@@ -57,14 +57,14 @@ public:
                the stack before traversing
               the node's left subtree */
             s.push(curr);
+            v.push_back(curr->val);
+            i++;
             cout << curr->val << " ";
             curr = curr->left;
         }
  
         /* Current must be NULL at this point */
         curr = s.top();
-        v.push_back(curr->val);
-        i++;
         s.pop();
  
 
