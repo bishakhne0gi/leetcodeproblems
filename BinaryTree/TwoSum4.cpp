e86ec53fb7c4487c092cc43686b51c03edd835f9
Bishakh Neogi <50258639+bishakhne0gi@users.noreply.github.com>
2022-02-13 22:48:37 +0530

    Binary Tree

---
+++ BinaryTree/TwoSum4.cpp
@@ -0,0 +1,63 @@
+/*
+Given the root of a Binary Search Tree and a target number k, return true if there exist two elements in the BST such that their sum is equal to the given target.
+
+ 
+
+Example 1:
+
+
+Input: root = [5,3,6,2,4,null,7], k = 9
+Output: true
+Example 2:
+
+
+Input: root = [5,3,6,2,4,null,7], k = 28
+Output: false
+*/
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        vector<int> nums;
+        inorder(root, nums);
+        return findTargetInSortedArray(nums, k);
+    }
+
+private:
+    void inorder(TreeNode* node, vector<int>& nums) {
+        if (!node) return;
+        inorder(node->left, nums);
+        nums.push_back(node->val);
+        inorder(node->right, nums);
+    }
+
+    bool findTargetInSortedArray(vector<int> a, int target) {
+        for (int i = 0, j = a.size() - 1; i < j;) {
+            int sum = a[i] + a[j];
+            if (sum == target) {
+                return true;
+            }
+            else if (sum < target) {
+                i++;
+            }
+            else {
+                j--;
+            }
+        }
+
+        return false;
+    }
+};
