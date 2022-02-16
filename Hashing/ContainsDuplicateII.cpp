9d02ce2494387736512b8aa83728c7253623634c
Bishakh Neogi <50258639+bishakhne0gi@users.noreply.github.com>
2022-02-16 23:03:43 +0530

    Hashtable

---
+++ Hashing/ContainsDuplicateII.cpp
@@ -0,0 +1,31 @@
+/*
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        for(int i = 0; i < nums.size(); i++) {
+            if(mp.count(nums[i])==1){
+                int diff = abs(mp[nums[i]] - i);
+                if (diff <= k)return true;  
+            }
+            mp[nums[i]] = i;
+        }
+        return false;
+    }
+};
+*/
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        for(int i = 0; i < nums.size(); i++) {
+            if(mp.count(nums[i])==1){
+                int diff = abs(mp[nums[i]] - i);
+                if (diff <= k)return true;  
+            }
+            mp[nums[i]] = i;
+        }
+        return false;
+    }
+};
